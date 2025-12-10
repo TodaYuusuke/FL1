63d15afc0c9904fc39ab3415ef8ded3843d9642b
humimotokousaku <115054913+humimotokousaku@users.noreply.github.com>
2025-12-10 15:56:25 +0900

    自機の移動制限(-250~250)

--- class/GameObjects/Player/Player.cpp
+++ class/GameObjects/Player/Player.cpp
@@ -141,6 +141,10 @@ void Player::Update() {
 
 	model_.worldTF.translation += moveController_->GetVel() + weaponVel_;
 
+	model_.worldTF.translation.x = std::clamp<float>(model_.worldTF.translation.x, -250.0f, 250.0f);
+	model_.worldTF.translation.y = std::clamp<float>(model_.worldTF.translation.y, -250.0f, 250.0f);
+	model_.worldTF.translation.z = std::clamp<float>(model_.worldTF.translation.z, -250.0f, 250.0f);
+
 	AdjustRotate();
 
 	weaponVel_ = { 0.0f,0.0f,0.0f };
